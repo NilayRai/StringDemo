6e37d6aea17284c55fb7fbd73782354e35b479fb
NilayRai <nilaykumarrai0@gmail.com>
2018-10-11 19:24:46 +0530

    Hello JAva

---
+++ StringDemo/src/main/java/com/string/demo/StringDemo.java
@@ -0,0 +1,10 @@
+package com.string.demo;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String[] str = { "Nilay", "Kumar", "Nihal", "Vikash" };
+		for (int i = 0; i < str.length; i++) {
+			System.out.println(str[i]);
+		}
+	}
+}
