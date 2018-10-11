c41b54caa1a22aaad1d8e3a8412c071eb4c5fba1
NilayRai <nilaykumarrai0@gmail.com>
2018-10-11 19:33:42 +0530

    Developer 1 changed

--- StringDemo/src/main/java/com/string/demo/StringDemo.java
+++ StringDemo/src/main/java/com/string/demo/StringDemo.java
@@ -2,9 +2,10 @@ package com.string.demo;
 
 public class StringDemo {
 	public static void main(String[] args) {
-		String[] str = { "Nilay", "Kumar", "Nihal", "Vikash" };
+		String[] str = { "Nilay", "Kumar", "Nihal", "Vikash", "Mahi" };
 		for (int i = 0; i < str.length; i++) {
 			System.out.println(str[i]);
+			System.out.println("Hello Java");
 		}
 	}
 }
